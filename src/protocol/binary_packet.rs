26508a7158f6f0c259b2c8c08eceb40dd702c02e
anko9801 <daiusa3@icloud.com>
2023-08-05 14:33:47 +0900

    create binary packet

--- src/protocol/binary_packet.rs
+++ src/protocol/binary_packet.rs
@@ -7,6 +7,7 @@ use nom::IResult;
 //   byte[n1]  payload; n1 = packet_length - padding_length - 1 Initially, compression MUST be "none".
 //   byte[n2]  random padding; n2 = padding_length
 //   byte[m]   mac (Message Authentication Code - MAC); m = mac_length Initially, the MAC algorithm MUST be "none".
+// mac = MAC(key, sequence_number || unencrypted_packet)
 pub struct BinaryPacket {
     packet_length: u32,
     padding_length: u8,
@@ -16,6 +17,19 @@ pub struct BinaryPacket {
 }
 
 impl BinaryPacket {
+    pub fn new(payload: Vec<u8>) -> Self {
+        let payload_length = payload.len() as u32;
+        let packet_length = (payload_length + 1) / 8 * 8;
+        let padding_length = (packet_length - packet_length) as u8;
+        BinaryPacket {
+            packet_length,
+            padding_length,
+            payload,
+            padding: vec![0; padding_length as usize],
+            mac: vec![],
+        }
+    }
+
     pub fn parse_binary_packet(input: &[u8]) -> IResult<&[u8], BinaryPacket> {
         let mac_length: usize = 0;
         let (input, packet_length) = be_u32(input)?;
@@ -34,5 +48,15 @@ impl BinaryPacket {
                 mac: mac.to_vec(),
             },
         ))
+    }
+
+    pub fn generate_binary_packet(&self) -> Vec<u8> {
+        let mut packet = vec![];
+        packet.extend(self.packet_length.to_be_bytes());
+        packet.extend(self.padding_length.to_be_bytes());
+        packet.extend(&self.payload);
+        packet.extend(&self.padding);
+        packet.extend(&self.mac);
+        packet
     }
 }
