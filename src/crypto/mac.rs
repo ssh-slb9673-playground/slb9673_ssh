247c8ccc79967411c1bfee92e85c73f458c5becb
anko9801 <daiusa3@icloud.com>
2023-08-05 19:19:19 +0900

    add sha2 in mac

--- src/crypto/mac.rs
+++ src/crypto/mac.rs
@@ -1,5 +1,6 @@
 use hmac::{Hmac, Mac};
 use sha1::Sha1;
+use sha2::{Sha256, Sha512};
 
 struct MAC {
     key: Vec<u8>,
@@ -10,10 +11,15 @@ struct MAC {
 // hmac-sha1-96 RECOMMENDED     first 96 bits of HMAC-SHA1 (digest length = 12, key length = 20)
 // hmac-md5     OPTIONAL        HMAC-MD5 (digest length = key length = 16)
 // hmac-md5-96  OPTIONAL        first 96 bits of HMAC-MD5 (digest length = 12, key length = 16)
+// hmac-sha2-256     RECOMMENDED   HMAC-SHA2-256 (digest length = 32 bytes, key length = 32 bytes)
+// hmac-sha2-512     OPTIONAL      HMAC-SHA2-512 (digest length = 64 bytes, key length = 64 bytes)
+
 // none         OPTIONAL        no MAC; NOT RECOMMENDED
 enum MacAlgorithm {
     hmac_sha1,
     hmac_sha1_96,
+    hmac_sha2_256,
+    hmac_sha2_512,
     hmac_md5,
     hmac_md5_96,
     none,
@@ -21,23 +27,26 @@ enum MacAlgorithm {
 
 impl MAC {
     pub fn generate_mac(&self, msg: Vec<u8>) -> Vec<u8> {
-        type HmacSha1 = Hmac<Sha1>;
         match self.algorithm {
             MacAlgorithm::hmac_sha1 => {
-                let mut mac =
-                    Hmac::<Sha1>::new_from_slice(&self.key).expect("HMAC can take key of any size");
+                let mut mac = Hmac::<Sha1>::new_from_slice(&self.key).unwrap();
                 mac.update(&msg);
-                let result = mac.finalize();
-                let code_bytes = result.into_bytes();
-                code_bytes.to_vec()
+                mac.finalize().into_bytes().to_vec()
             }
             MacAlgorithm::hmac_sha1_96 => {
-                let mut mac =
-                    Hmac::<Sha1>::new_from_slice(&self.key).expect("HMAC can take key of any size");
+                let mut mac = Hmac::<Sha1>::new_from_slice(&self.key).unwrap();
                 mac.update(&msg);
-                let result = mac.finalize();
-                let code_bytes = result.into_bytes();
-                code_bytes.to_vec()
+                mac.finalize().into_bytes().to_vec()
+            }
+            MacAlgorithm::hmac_sha2_256 => {
+                let mut mac = Hmac::<Sha256>::new_from_slice(&self.key).unwrap();
+                mac.update(&msg);
+                mac.finalize().into_bytes().to_vec()
+            }
+            MacAlgorithm::hmac_sha2_512 => {
+                let mut mac = Hmac::<Sha512>::new_from_slice(&self.key).unwrap();
+                mac.update(&msg);
+                mac.finalize().into_bytes().to_vec()
             }
             _ => vec![],
         }
