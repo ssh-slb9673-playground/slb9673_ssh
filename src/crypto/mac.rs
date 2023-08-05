d64ce709f04834169604857350fa05a695b1f237
anko9801 <daiusa3@icloud.com>
2023-08-05 19:24:28 +0900

    fix mac

--- src/crypto/mac.rs
+++ src/crypto/mac.rs
@@ -2,18 +2,12 @@ use hmac::{Hmac, Mac};
 use sha1::Sha1;
 use sha2::{Sha256, Sha512};
 
-struct MAC {
-    key: Vec<u8>,
-    algorithm: MacAlgorithm,
-}
-
 // hmac-sha1    REQUIRED        HMAC-SHA1 (digest length = key length = 20)
 // hmac-sha1-96 RECOMMENDED     first 96 bits of HMAC-SHA1 (digest length = 12, key length = 20)
 // hmac-md5     OPTIONAL        HMAC-MD5 (digest length = key length = 16)
 // hmac-md5-96  OPTIONAL        first 96 bits of HMAC-MD5 (digest length = 12, key length = 16)
 // hmac-sha2-256     RECOMMENDED   HMAC-SHA2-256 (digest length = 32 bytes, key length = 32 bytes)
 // hmac-sha2-512     OPTIONAL      HMAC-SHA2-512 (digest length = 64 bytes, key length = 64 bytes)
-
 // none         OPTIONAL        no MAC; NOT RECOMMENDED
 enum MacAlgorithm {
     hmac_sha1,
@@ -25,7 +19,16 @@ enum MacAlgorithm {
     none,
 }
 
+struct MAC {
+    key: Vec<u8>,
+    algorithm: MacAlgorithm,
+}
+
 impl MAC {
+    pub fn new(key: Vec<u8>, algorithm: MacAlgorithm) -> Self {
+        MAC { key, algorithm }
+    }
+
     pub fn generate_mac(&self, msg: Vec<u8>) -> Vec<u8> {
         match self.algorithm {
             MacAlgorithm::hmac_sha1 => {
