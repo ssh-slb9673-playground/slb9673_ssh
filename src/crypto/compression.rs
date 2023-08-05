7af7cdfe19266d1d67a5a5be5f7eb5d29205c959
anko9801 <daiusa3@icloud.com>
2023-08-05 17:15:01 +0900

    add compression

--- src/crypto/compression.rs
+++ src/crypto/compression.rs
@@ -1,6 +1,40 @@
+use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use std::io::{prelude::*, Result};
+
 // none     REQUIRED        no compression
 // zlib     OPTIONAL        ZLIB (LZ77) compression
-enum Compression {
+enum CompressionAlgorithm {
     Zlib,
     None,
+}
+
+pub struct Compress {
+    algorithm: CompressionAlgorithm,
+}
+
+impl Compress {
+    pub fn compress(&self, msg: Vec<u8>) -> Result<Vec<u8>> {
+        match self.algorithm {
+            CompressionAlgorithm::Zlib => {
+                let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+                e.write_all(msg.as_slice())?;
+                e.finish()
+            }
+            CompressionAlgorithm::None => Ok(msg),
+        }
+    }
+
+    pub fn decompress(&self, msg: Vec<u8>) -> Result<Vec<u8>> {
+        match self.algorithm {
+            CompressionAlgorithm::Zlib => {
+                let mut d = ZlibDecoder::new(msg.as_slice());
+                let mut result = vec![];
+                d.read(&mut result)?;
+                Ok(result)
+            }
+            CompressionAlgorithm::None => Ok(msg),
+        }
+    }
 }
