39c209e32fbcc86377a67ee13db62a2d0bc7991f
Daiki Usami <daiusa3@icloud.com>
2023-08-08 17:19:35 +0900

    add curve25519sha256

--- src/crypto/key_exchage.rs
+++ src/crypto/key_exchage.rs
@@ -9,7 +9,7 @@ use x25519_dalek::{EphemeralSecret, PublicKey};
 // curve448-sha512
 pub trait KexMethod {
     fn public_key(&self) -> Vec<u8>;
-    fn shared_secret(&self, public_key: &[u8]) -> Vec<u8>;
+    fn shared_secret(&mut self, public_key: &[u8]) -> Vec<u8>;
     fn hash(&self, seed: &[u8]) -> Vec<u8>;
 }
 
@@ -23,7 +23,7 @@ impl KexMethod for DiffieHellmanGroup1Sha1 {
     fn public_key(&self) -> Vec<u8> {
         vec![]
     }
-    fn shared_secret(&self, public_key: &[u8]) -> Vec<u8> {
+    fn shared_secret(&mut self, public_key: &[u8]) -> Vec<u8> {
         vec![]
     }
     fn hash(&self, seed: &[u8]) -> Vec<u8> {
@@ -43,7 +43,7 @@ impl KexMethod for DiffieHellmanGroup14Sha1 {
     fn public_key(&self) -> Vec<u8> {
         vec![]
     }
-    fn shared_secret(&self, public_key: &[u8]) -> Vec<u8> {
+    fn shared_secret(&mut self, public_key: &[u8]) -> Vec<u8> {
         vec![]
     }
     fn hash(&self, seed: &[u8]) -> Vec<u8> {
@@ -54,7 +54,7 @@ impl KexMethod for DiffieHellmanGroup14Sha1 {
 }
 
 pub struct Curve25519Sha256 {
-    private_key: EphemeralSecret,
+    private_key: Option<EphemeralSecret>,
     public_key: PublicKey,
 }
 impl Curve25519Sha256 {
@@ -62,17 +62,21 @@ impl Curve25519Sha256 {
         let private_key = EphemeralSecret::random_from_rng(OsRng);
         let public_key = PublicKey::from(&private_key);
         Curve25519Sha256 {
-            private_key,
+            private_key: Some(private_key),
             public_key,
         }
     }
 }
 impl KexMethod for Curve25519Sha256 {
     fn public_key(&self) -> Vec<u8> {
-        vec![]
+        self.public_key.to_bytes().to_vec()
     }
-    fn shared_secret(&self, public_key: &[u8]) -> Vec<u8> {
-        vec![]
+    fn shared_secret(&mut self, public_key: &[u8]) -> Vec<u8> {
+        let public_key: [u8; 32] = public_key.try_into().unwrap();
+        let public_key = PublicKey::from(public_key);
+        let private_key = self.private_key.take().unwrap();
+        let shared_secret = private_key.diffie_hellman(&public_key);
+        shared_secret.to_bytes().to_vec()
     }
     fn hash(&self, seed: &[u8]) -> Vec<u8> {
         let mut hasher = Sha256::new();
@@ -91,7 +95,7 @@ impl KexMethod for Curve448Sha512 {
     fn public_key(&self) -> Vec<u8> {
         vec![]
     }
-    fn shared_secret(&self, public_key: &[u8]) -> Vec<u8> {
+    fn shared_secret(&mut self, public_key: &[u8]) -> Vec<u8> {
         vec![]
     }
     fn hash(&self, seed: &[u8]) -> Vec<u8> {
