ed7423609ccda4533d5e851a035c304df736fb13
anko9801 <daiusa3@icloud.com>
2024-02-18 01:12:04 +0900

    :art: Divide channel into 2 files

--- src/protocol/channel/channel.rs
+++ src/protocol/channel/channel.rs
@@ -1,17 +1,13 @@
-use crate::protocol::{
-    client::SshClient,
-    data::{ByteString, Data},
-    ssh2::message_code,
-};
+use crate::protocol::{client::SshClient, data::Data, ssh2::message_code};
 use anyhow::Result;
 
 pub struct Channel<'a> {
     client: &'a mut SshClient,
-    client_channel: u32,
-    server_channel: u32,
-    channel_type: String,
-    initial_window_size: u32,
-    maximum_packet_size: u32,
+    pub client_channel: u32,
+    pub server_channel: u32,
+    pub channel_type: String,
+    pub initial_window_size: u32,
+    pub maximum_packet_size: u32,
 }
 
 impl<'a> Channel<'a> {
@@ -24,117 +20,20 @@ impl<'a> Channel<'a> {
     }
 
     pub fn client_setup(&mut self) -> Result<()> {
-        self.channel()?;
-        self.channel()?;
+        if self.channel()? == message_code::SSH_MSG_GLOBAL_REQUEST {
+            println!("");
+        }
+
+        if self.channel()? == message_code::SSH_MSG_DEBUG {
+            println!("");
+        }
+
         self.send_channel_open()?;
+
         if self.channel()? != message_code::SSH_MSG_CHANNEL_OPEN_CONFIRMATION {
             panic!("test")
         }
-        Ok(())
-    }
 
-    pub fn send_channel_open(&mut self) -> Result<()> {
-        self.send(
-            &Data::new()
-                .put(&message_code::SSH_MSG_CHANNEL_OPEN)
-                .put(&self.channel_type)
-                .put(&self.client_channel)
-                .put(&self.initial_window_size)
-                .put(&self.maximum_packet_size),
-        )
-    }
-
-    pub fn channel_open_confirmation(&mut self, payload: &mut Data) -> Result<()> {
-        let recipient_channel: u32 = payload.get();
-        let sender_channel: u32 = payload.get();
-        let initial_window_size: u32 = payload.get();
-        let maximum_packet_size: u32 = payload.get();
-        println!(
-            "{} {} {} {}",
-            recipient_channel, sender_channel, initial_window_size, maximum_packet_size
-        );
-        match self.channel_type.as_str() {
-            "session" => {}
-            "x11" => {
-                let originator_address: String = payload.get();
-                let originator_port: u32 = payload.get();
-                println!("{}:{}", originator_address, originator_port);
-            }
-            "forwarded-tcpip" => {
-                let address: String = payload.get();
-                let port: u32 = payload.get();
-                let originator_address: String = payload.get();
-                let originator_port: u32 = payload.get();
-                println!("old: {}:{}", address, port);
-                println!("new: {}:{}", originator_address, originator_port);
-            }
-            "direct-tcpip" => {
-                let host: String = payload.get();
-                let port: u32 = payload.get();
-                let originator_address: String = payload.get();
-                let originator_port: u32 = payload.get();
-                println!("old: {}:{}", host, port);
-                println!("new: {}:{}", originator_address, originator_port);
-            }
-            _ => {}
-        }
-        Ok(())
-    }
-
-    pub fn shell(&mut self) -> Result<()> {
-        let env: String = "".to_string();
-        let terminal_width_characters: u32 = 0;
-        let terminal_height_rows: u32 = 0;
-        let terminal_width_pixels: u32 = 0;
-        let terminal_height_pixels: u32 = 0;
-        let encoded_terminal_modes: ByteString = ByteString(vec![
-            128, // TTY_OP_ISPEED
-            0, 1, 0xc2, 0,   // 115200
-            129, // TTY_OP_OSPEED
-            0, 1, 0xc2, 0,    // 115200 again
-            0_u8, // TTY_OP_END
-        ]);
-        let data = Data::new()
-            .put(&message_code::SSH_MSG_CHANNEL_REQUEST)
-            .put(&self.server_channel)
-            .put(&"pty-req".to_string())
-            .put(&false)
-            .put(&env)
-            .put(&terminal_width_characters)
-            .put(&terminal_height_rows)
-            .put(&terminal_width_pixels)
-            .put(&terminal_height_pixels)
-            .put(&encoded_terminal_modes);
-        self.send(&data)?;
-
-        let data = Data::new()
-            .put(&message_code::SSH_MSG_CHANNEL_REQUEST)
-            .put(&self.server_channel)
-            .put(&"shell".to_string())
-            .put(&true);
-        self.send(&data)?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
-        Ok(())
-    }
-
-    pub fn exec(&mut self, command: String) -> Result<()> {
-        println!("exec: {}", command);
-        let data = Data::new()
-            .put(&message_code::SSH_MSG_CHANNEL_REQUEST)
-            .put(&self.server_channel)
-            .put(&"exec".to_string())
-            .put(&true)
-            .put(&command);
-        self.send(&data)?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
-        self.channel()?;
         Ok(())
     }
 
@@ -198,62 +97,6 @@ impl<'a> Channel<'a> {
         }
         Ok(message_code)
     }
-
-    fn debug(&mut self, payload: &mut Data) {
-        let want_reply: bool = payload.get();
-        let debug: String = payload.get();
-        println!("debug: {}", debug);
-        println!("reply: {}", want_reply);
-    }
-
-    fn global_request(&mut self, payload: &mut Data) {
-        let request_name: String = payload.get();
-        let want_reply: bool = payload.get();
-        println!("request: {}, reply: {}", request_name, want_reply);
-        match request_name.as_str() {
-            "tcpip-forward" => {
-                let address: String = payload.get();
-                let port: u32 = payload.get();
-                println!("{}:{}", address, port);
-            }
-            "cancel-tcpip-forward" => {
-                let address: String = payload.get();
-                let port: u32 = payload.get();
-                println!("{}:{}", address, port);
-            }
-            "hostkeys-00@openssh.com" => {
-                // let blob: ByteString = payload.get();
-            }
-            _ => {}
-        }
-    }
-    // fn recv_message_request_success(&mut self, payload: &mut Data) {
-    //     pub const FILE_CHUNK: usize = 30000;
-    //     let port: u32 = payload.get();
-    //     data.put_u8(ssh_msg_code::SSH_MSG_CHANNEL_REQUEST)
-    //         .put_u32(self.server_channel_no)
-    //         .put_str(ssh_str::PTY_REQ)
-    //         .put_u8(false as u8)
-    //         .put_str(ssh_str::XTERM_VAR)
-    //         .put_u32(tvs.0)
-    //         .put_u32(tvs.1)
-    //         .put_u32(tvs.2)
-    //         .put_u32(tvs.3);
-    //     let model = [
-    //         128, // TTY_OP_ISPEED
-    //         0, 1, 0xc2, 0,   // 115200
-    //         129, // TTY_OP_OSPEED
-    //         0, 1, 0xc2, 0,    // 115200 again
-    //         0_u8, // TTY_OP_END
-    //     ];
-    //     data.put_u8s(&model);
-    //     let mut data = Data::new();
-    //     data.put(&message_code::SSH_MSG_CHANNEL_REQUEST)
-    //         .put(self.server_channel_no)
-    //         .put("shell".to_string())
-    //         .put(false);
-    //     self.send(data)
-    // }
 }
 
 impl SshClient {
